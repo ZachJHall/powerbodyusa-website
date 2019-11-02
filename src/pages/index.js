47b6bc04448153da167f26051749ea99aa25648f
ZachJHall <zachjohnhall@gmail.com>
2019-11-02 15:28:30 -0500

    Imported and added image banner to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,27 @@
 import React from "react"
 import Layout from '../components/layout'
-
+import BannerImage from '../images/powerbodyusa_meet_image.jpg'
 
 const IndexPage = () => {
   return (
     <Layout>
-      <h1>About PowerBody USA Fitness</h1>
-      <p>*POWERBODY USA is family-owned and operated by husband and wife team Strength Coach Chuck and Former National and World Champion Powerlifter, Carla Trosper.
-      Since opening in 1996, we treat every member like they are a part of our family.
-      Our primary goal is to share our 50 plus years of fitness knowledge with our community.
-      Other companies may offer similar services, but our services come with a personal touch with affordable
-       training sessions and reasonable yearly and short term memberships.
-      Regardless of your fitness level, we can generate a plan that works for you...and age is never an excuse!
-      </p>
+
+      <div class="banner-image">
+        <img src={ BannerImage } alt="Competition Team Photo" />
+      </div>
+
+      <div class="index-content">
+
+        <h1>About PowerBody USA Fitness</h1>
+        <p>*POWERBODY USA is family-owned and operated by husband and wife team Strength Coach Chuck and Former National and World Champion Powerlifter, Carla Trosper.
+        Since opening in 1996, we treat every member like they are a part of our family.
+        Our primary goal is to share our 50 plus years of fitness knowledge with our community.
+        Other companies may offer similar services, but our services come with a personal touch with affordable
+         training sessions and reasonable yearly and short term memberships.
+        Regardless of your fitness level, we can generate a plan that works for you...and age is never an excuse!</p>
+
+      </div>
+      
     </Layout>
   )
 }
