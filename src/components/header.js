b681a67bd9ff73feecb1d4264352b2bb2d138415
ZachJHall <zachjohnhall@gmail.com>
2019-11-02 13:47:54 -0500

    Added a header component with link to about us

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+const Header = () => {
+  return (
+    <div class="header-links">
+    <Link to="/about_us/">About Us</Link>
+    </div>
+  )
+}
+
+export default Header
