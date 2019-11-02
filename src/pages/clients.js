b58d17c87824d11924cb8ad22c87b940349af1eb
ZachJHall <zachjohnhall@gmail.com>
2019-11-02 16:04:32 -0500

    Added content to client page

--- src/pages/clients.js
+++ src/pages/clients.js
@@ -5,7 +5,18 @@ import Layout from '../components/layout'
 const IndexPage = () => {
   return (
     <Layout>
-      <h1>Clients</h1>
+
+      <div class="clients-content">
+
+        <h1 id="clients-header-title">About PowerBody USA Fitness</h1>
+
+        <p>Step into PowerBody USA and enjoy our secured 24-hour keypass facility where we specialize in strength training for all ages, gender, and fitness levels.
+        Whether you are a walk-in member or a personal trainee we are here to help you begin or take the next step towards a fitness and strength program that agrees with the needs of your lifestyle.
+        You may want to make speed and strength improvements for a sport or just have a desire to get back into shape,
+        everyone benefits from a fitness program and we will help provide each individual with the tools needed to be successful with obtaining realistic fitness goals.</p>
+        
+      </div>
+
     </Layout>
   )
 }
