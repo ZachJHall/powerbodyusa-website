ef94363575cde905afd144c0dc585358a2d9024f
ZachJHall <zachjohnhall@gmail.com>
2019-11-02 16:15:53 -0500

    Created a base css file and imported it into the layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from './header'
+import '../css/base.css'
 
 const Layout = (props) => {
   return (
     <div>
+
       <Header/>
 
       {props.children}
+
     </div>
   )
 }
