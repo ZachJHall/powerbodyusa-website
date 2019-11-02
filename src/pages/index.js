0e81223dbc2bae6d5039252f523cb8b2926a155c
ZachJHall <zachjohnhall@gmail.com>
2019-11-02 13:43:10 -0500

    Added component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,13 @@
 import React from "react"
+import Layout from '../components/layout'
 
-export default () => <div>Test!</div>
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      <h1>Test</h1>
+    </Layout>
+  )
+}
+
+export default IndexPage
