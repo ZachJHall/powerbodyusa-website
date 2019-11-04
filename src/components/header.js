f3467a4de60a8d0a3c38f8bd822667c83a96f9b0
ZachJHall <zachjohnhall@gmail.com>
2019-11-04 12:24:21 -0600

    Organized header.js

--- src/components/header.js
+++ src/components/header.js
@@ -4,9 +4,13 @@ import { Link } from 'gatsby';
 const Header = () => {
   return (
     <div class="header-links">
-    <Link to="/">Home</Link>
-    <Link to="/about/">About</Link>
+
+      <Link to="/">Home</Link>
+
+      <Link to="/about/">About</Link>
+      
       <Link to="/clients/">Clients</Link>
+
     </div>
   )
 }
