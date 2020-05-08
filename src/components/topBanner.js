90a75e7912c5d5328979a2e8d7f7d425b0da0e75
ZachJHall <zachjohnhall@gmail.com>
2020-05-08 00:57:42 -0500

    Centered text on topBanner and added contact Us button

--- src/components/topBanner.js
+++ src/components/topBanner.js
@@ -10,17 +10,21 @@ const TopBanner = () => {
       <div class="topBanner">
           <img src={TopBannerImage} />
 
-          <div class="topBannerQuote">
 
+          <div class="topBannerQuote">
             <div class="topBannerQuoteContent">
+
               <div class="topBannerQuoteContentTop">
-                <div>REAL GYM.</div>
-                <div>REAL RESULTS.</div>
+                <h1>REAL GYM.</h1> <h1>REAL RESULTS.</h1>
               </div>
 
-              <div class="topBannerQuoteContentBottom"> <button>Contact Us</button></div>
+              <div class="topBannerQuoteContentBottom">
+                <a href="tel:+16362818120"><button>Contact Us</button></a>
+              </div>
+
             </div>
           </div>
+
       </div>
 
 
