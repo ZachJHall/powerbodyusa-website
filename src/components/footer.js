e12fc6f712a1633c5c705b0aac15c7228a477a60
JJ-Hall <joshhall1991@gmail.com>
2020-05-08 02:53:09 -0500

    footer added

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,6 +2,7 @@ import React from "react"
 import '../css/footer.css'
 import FbIcon from '../images/fbIcon.svg'
 import InstaIcon from '../images/instaIcon.svg'
+import PhoneIcon from '../images/phoneIcon.svg'
 
 
 const Footer = () => {
@@ -18,11 +19,16 @@ const Footer = () => {
           </div>
 
           <div className="footer-center">
-
+            <h3>Call Us Today!</h3>
+            <div className="footer-phoneIcon">
+              <img src={PhoneIcon} />
+              <h3>636-281-8120</h3>
+            </div>
           </div>
 
           <div className="footer-right">
-            
+            <h3>Address:</h3>
+            <h3>403 Sonderen St, O'Fallon, MO 63366</h3>
           </div>
 
       </div>
