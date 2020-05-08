7fe54513076cf613de3574187fb8faf5e122d339
JJ-Hall <joshhall1991@gmail.com>
2020-05-08 00:58:37 -0500

    updated footer-left

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,29 @@
 import React from "react"
 import '../css/footer.css'
+import FbIcon from '../images/fbIcon.svg'
+import InstaIcon from '../images/instaIcon.svg'
 
 
 const Footer = () => {
   return (
 
       <div class="footer-div">
+
+          <div className="footer-left">
+              <h3>Connect With Us!</h3>
+              <div className="footer-icons">
+                <img src={FbIcon} />
+                <img src={InstaIcon} />
+              </div>
+          </div>
+
+          <div className="footer-center">
+
+          </div>
+
+          <div className="footer-right">
+            
+          </div>
 
       </div>
 
