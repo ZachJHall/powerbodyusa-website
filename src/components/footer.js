6b951f06dc5aecb33c72fe86f8d21bfa5dea2df4
ZachJHall <zachjohnhall@gmail.com>
2020-05-07 23:00:53 -0500

    Added footer

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React from "react"
+import '../css/footer.css'
+
+
+const Footer = () => {
+  return (
+
+      <div class="footer-div">
+
+      </div>
+
+  )
+}
+
+export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,13 @@ import '../css/index.css'
 import Layout from '../components/layout'
 
 import TopBanner from '../components/topBanner'
-
+import Footer from '../components/footer'
 
 const IndexPage = () => {
   return (
     <Layout>
       <TopBanner />
+      <Footer />
 
 
     </Layout>
