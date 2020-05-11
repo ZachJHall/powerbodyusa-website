a8e679abd2181a392ebe1feadc83e9e726590fa0
ZachJHall <zachjohnhall@gmail.com>
2020-05-11 00:12:00 -0500

    Added content to flexible options card

--- src/components/about.js
+++ src/components/about.js
@@ -14,7 +14,7 @@ const About = () => {
 
         <div class="aboutCardContainer">
           <InfoCard title = "Variety" items = {["Weight Equipment","Steam Room","Suana", "Mini Track"]} />
-          <InfoCard title = "Flexible Options" items = {["Personal Training","Group Classes","Nutrition Assistance", "Flexible Payment Plans"]} />
+          <InfoCard title = "Flexible Options" items = {["24/7 Access", "Personal Training","Group Classes","Nutrition Assistance", "Flexible Payment Plans"]} />
           <InfoCard title = "Community" items = {["High Level Athletes","Friendly Supportive Environment","Competition Hosting"]} />
 
         </div>
