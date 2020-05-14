5a3649fea54de65d069aee0133db9ee86c2badce
JJ-Hall <joshhall1991@gmail.com>
2020-05-13 21:45:30 -0500

    Zach cant spell

--- src/components/about.js
+++ src/components/about.js
@@ -15,7 +15,7 @@ const About = () => {
         <h1>Why PowerBody USA</h1> <h1> Fitness?</h1>
         </div>
         <div class="aboutCardContainer">
-          <InfoCard  src = {varietyPic} alt = 'Trainer training young client to curl at Powerbody USA' title = "Variety" items = {["Free Weights", "Cable Machines", "Steam Room","Suana", "Mini Track"]} />
+          <InfoCard  src = {varietyPic} alt = 'Trainer training young client to curl at Powerbody USA' title = "Variety" items = {["Free Weights", "Cable Machines", "Steam Room","Sauna", "Mini Track"]} />
           <InfoCard  src = {flexiblePic} alt = 'Trainer training client to squat at PowerBody USA' title = "Flexible Options" items = {["24/7 Access", "Personal Training","Group Classes","Nutrition Assistance", "Flexible Payment Plans"]} />
           <InfoCard src = {communityPic} alt = 'Three women lifting a barbell at Powerbody USA' title = "Community" items = {["High Level Athletes","Friendly Supportive Environment","Competition Hosting"]} />
 
