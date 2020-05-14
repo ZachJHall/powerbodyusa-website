e4f898bc9196058cb466f7f88502c9d82a862731
ZachJHall <zachjohnhall@gmail.com>
2020-05-13 22:42:59 -0500

    Added react-helmet and gatsby-plugin-react-helmet and implemented page title

---
+++ src/components/helmetcomponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Helmet } from "react-helmet"
+
+
+const HelmetComponent = () => {
+  return (
+
+      <Helmet>
+        <title>PowerBody USA</title>
+      </Helmet>
+
+  )
+}
+
+export default HelmetComponent
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,17 @@ import React from "react"
 import '../css/index.css'
 import Layout from '../components/layout'
 
+import HelmetComponent from '../components/helmetcomponent'
 import TopBanner from '../components/topBanner'
 import About from '../components/about'
 import Footer from '../components/footer'
 
+
+
 const IndexPage = () => {
   return (
     <html lang='en'>
+      <HelmetComponent />
       <Layout>
         <TopBanner />
         <About />
